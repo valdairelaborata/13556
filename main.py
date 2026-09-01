ec05260957b60f6f45e16f621acb750ceadb80bf
Your Name <you@example.com>
2026-08-31 21:01:49 -0300

    poo

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 
 class Carro:
     def __init__(self, cor, placa, status, tipo):
-        pass
+        self.cor = cor
+        self.placa = placa
+        self.status = status
+        self.tipo = tipo
 
+    
     def ligar(self):
-        pass
+        self.status = "ligado"
 
     def desligar(self):
-        pass
+        self.status = "Desligado"
+
+
+
+celta = Carro("Vermelha", "AKH 2522", "desligado", "hatch")
+fusca = Carro("Preta", "XTO 2536", "Ligado", "1500")
+celta.ligar()
+fusca.desligar()
