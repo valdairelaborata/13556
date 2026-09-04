e3bfb3bf012e8ed6cbe2439f65a19e6dfe2b22b6
Your Name <you@example.com>
2026-09-04 20:00:20 -0300

    metodos

--- main_v2.py
+++ main_v2.py
@@ -9,6 +9,10 @@ class Carro:
     def velocidade(self):
         return self.__velocidade
 
+    @property
+    def nivel_combustivel(self):
+        return self.__verificar_combustivel()
+
     @velocidade.setter
     def velocidade(self, valor):
         if valor >= 0:
@@ -18,13 +22,20 @@ class Carro:
         
 
     def acelerar(self, valor):
-        self.velocidade = valor
+        self.velocidade += valor
 
+    def frear(self, valor):
+        self.__velocidade -= valor
+
+    def __verificar_combustivel(self):
+        return "10L"
 
 meu_carro = Carro("Toyota", "Corola")
 meu_carro.acelerar(10)
+meu_carro.frear(2)
 
 
-print(meu_carro.velocidade)
+
+print(meu_carro.nivel_combustivel)
 
 
