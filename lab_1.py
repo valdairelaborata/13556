1e54434487ce86ec353812be96456d7435330091
Your Name <you@example.com>
2026-09-10 19:19:28 -0300

    lab_01

--- lab_1.py
+++ lab_1.py
@@ -1,46 +1,7 @@
-# 1- Considere uma classe de Conta Bancária e aplique os conceitos de encapsulamento para movimentar o saldo e a troca do titular.
+# 1- Crie uma classe chamada ContaBancaria que represente uma conta bancária básica. A conta deve ter um número de conta, um titular da conta e um saldo. Implemente os seguintes métodos mágicos:
+# __init__(self, numero, titular, saldo): O construtor que inicializa os atributos da conta.
+# __str__(self): O método que retorna uma representação em string da conta no formato "Conta de [titular]: [saldo]".
 
-class ContaBancaria:
-    def __init__(self, titular:str):
-        self.__titular = titular
-        self.__saldo = 0
-
-    @property
-    def saldo(self):
-        return self.__saldo
-
-    @property
-    def titular(self):
-        return self.__titular
-
-
-    @titular.setter
-    def titular(self, novo_titular):
-         self.__titular = novo_titular
-
-
-    def depositar(self, valor):
-        self.__saldo += valor
- 
-    def sacar(self, valor):
-        if valor <= self.__saldo:        
-            self.__saldo -= valor
-        
-
-
-
-
-conta_01 = ContaBancaria("Valdair")
-
-print(f'{conta_01.titular} - {conta_01.saldo}')
-
-conta_01.depositar(10)
-conta_01.depositar(10)
-conta_01.depositar(10)
-
-conta_01.sacar(5)
-
-conta_01.titular = "Ana"
-
-print(f'{conta_01.titular} - {conta_01.saldo}')
-
+# Além dos métodos mágicos, implemente também os seguintes métodos:
+# depositar(self, valor): Adiciona um valor ao saldo da conta.
+# sacar(self, valor): Retira um valor do saldo da conta, desde que haja saldo suficiente..
