793bb6d70cdaf63cc6101bffcf88919a82ac5242
Your Name <you@example.com>
2026-09-14 21:24:31 -0300

    teste

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import requests
+
+import unittest
+
+class Calculadora:
+    def soma(self, a, b):        
+        return a + b
+
+    def soma_com_valo_adicional(self, a, b):
+        valor_adicional = 2
+        return self.soma(a, b) + valor_adicional
 
 
-url = "https://jsonplaceholder.typicode.com/posts/1"
+class TesteCalculadora(unittest.TestCase):
 
-response = requests.get(url)
+    def test_soma(self):
+        calcculadora = Calculadora()
+        resultado = calcculadora.soma(2, 3)
+        self.assertEqual(resultado, 5)
 
-if response.status_code == 200:
-    data = response.json()
-    print(f'Título: {data["title"]}')   
-    
-else:
-    print(f"Erro. Status code: {response.status_code}")
+
+    def test_soma_com_valor_adicional(self):
+        calculadora = Calculadora()
+        resultado = calculadora.soma_com_valo_adicional(2, 3)
+        self.assertEqual(resultado, 7)  # Espera-se que o resultado seja 7 devido ao valor adicional de 2
+
+unittest.main()
+   
