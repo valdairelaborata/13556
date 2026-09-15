7a81f2792cd49b96e0ff1bfbda80a48388572e8f
Your Name <you@example.com>
2026-09-15 20:18:06 -0300

    lab_01

--- jvvt.py
+++ jvvt.py
@@ -1,10 +1,51 @@
-import requests
+import unittest
  
-url = 'https://viacep.com.br/ws/82640000/json/'
+class ContaBancaria:
+    def __init__(self, titular:str):
+        self.__titular = titular
+        self.__saldo = 0
+   
+    @property
+    def saldo(self):
+        return self.__saldo
+   
+    def depositar(self, valor):
+        if valor <= 0:
+             return "Valor inválido"
+        else:
+            self.__saldo += valor
  
-response = requests.get(url)
+    def sacar(self, valor):
+        if valor <= self.__saldo:
+            self.__saldo -= valor
+        else:
+            print("Saldo insuficiente")
  
-if response.status_code == 200:
-    data = response.json()
-    print(f'CEP: {data["cep"]}')    
-
+class TestContaBancaria(unittest.TestCase):
+ 
+    def test_saldo(self):
+        conta = ContaBancaria('João')
+        self.assertEqual(self.saldo, 0)
+ 
+    def test_depositar_valor_positivo(self):
+        conta = ContaBancaria('João')
+        novo_saldo = self.depositar(50)
+        self.assertEqual(novo_saldo, 50)
+       
+    def test_depositar_valo_invalido(self):
+        conta = ContaBancaria('João')
+        resultado = self.depositar(-10)
+        self.assertEqual(resultado, "Valor inválido")
+ 
+    def test_sacar_valor_valido(self):
+        conta = ContaBancaria('João')
+        novo_saldo = self.sacar(20)
+        self.assertEqual(novo_saldo, 30)
+ 
+    def test_sacar_valor_invalido(self):
+        conta = ContaBancaria('João')
+        self.sacar(-1)
+ 
+    def test_sacar_saldo_insuficiente(self):
+        conta = ContaBancaria('João')
+        self.sacar(2000)
--- lab_1.py
+++ lab_1.py
@@ -1,19 +1,80 @@
+# 1 - Crie uma classe ContaBancaria que permita depósitos, saques e verificação de saldo. Escreva testes para os métodos dessa classe.
 
-import requests
- 
-cep = "81710230"
-url = f"https://viacep.com.br/ws/{cep}/json/"
 
-resposta = requests.get(url)
-dados = resposta.json()
- 
-if resposta.status_code == 200:
-    dados = resposta.json()
-    print("--- DADOS DO ENDEREÇO ---")
-    print("Rua: " + dados['logradouro'])
-    print("Bairro: " + dados['bairro'])
-    print("Cidade: " + dados['localidade'])
-    print("UF: " + dados['uf'])
+import unittest
 
-else:
-    print(f"Erro. Status code: {resposta.status_code}") 
+
+class ContaBancaria:
+    def __init__(self, titular: str):
+        self.__titular = titular
+        self.__saldo = 0
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    def depositar(self, valor):
+        if valor <= 0:
+            return 'Valor invalido'
+        self.__saldo += valor
+
+    def sacar(self, valor):         
+        if valor <= self.__saldo:
+            self.__saldo -= valor
+        else:
+            return 'Saldo insuficiente'
+      
+
+class Teste_ContaBancaria(unittest.TestCase):
+
+    def test_depositar(self):
+        conta = ContaBancaria("João")
+        conta.depositar(50)
+        self.assertEqual(conta.saldo, 50)
+
+    def test_sacar(self):
+        conta = ContaBancaria("João")
+        conta.depositar(100)
+        conta.sacar(50)        
+        self.assertEqual(conta.saldo, 50)
+
+    def test_sacar_valor_insuficiente(self):
+        conta = ContaBancaria("João")
+        conta.depositar(100)
+        resultado = conta.sacar(150)
+        self.assertEqual(resultado, 'Saldo insuficiente')
+        self.assertEqual(conta.saldo, 100) 
+
+unittest.main()
+
+
+
+    # def test_saldo_inicial(self):
+    #     conta = ContaBancaria("João")
+    #     self.assertEqual(conta.saldo, 0)
+
+    # def test_depositar_valor_positivo(self):
+    #     conta = ContaBancaria("João")
+    #     conta.depositar(100)
+    #     self.assertEqual(conta.saldo, 100)
+
+    # def test_depositar_valor_invalido(self):
+    #     conta = ContaBancaria("João")
+    #     resultado = conta.depositar(-50)
+    #     self.assertEqual(resultado, 'Valor invalido')
+
+    # def test_sacar_valor_valido(self):
+    #     conta = ContaBancaria("João")
+    #     conta.depositar(100)
+    #     conta.sacar(50)
+    #     self.assertEqual(conta.saldo, 50)
+
+    # def test_sacar_valor_invalido(self):
+    #     conta = ContaBancaria("João")
+    #     resultado = conta.sacar(-10)
+    #     self.assertEqual(resultado, 'Saldo insuficiente')
+
+    # def test_sacar_saldo_insuficiente(self):
+    #     conta = ContaBancaria("João")
+    #     resultado = conta.sacar(2000)
+    #     self.assertEqual(resultado, 'Saldo insuficiente para saque')
