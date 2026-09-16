69c16fec94d914d25ab5142681f3ef690126e1ba
Your Name <you@example.com>
2026-09-16 20:13:00 -0300

    orm

--- jvvt.py
+++ jvvt.py
@@ -1,58 +1,9 @@
-import unittest
+from sqlalchemy import create_engine
+
+from jvvtmodel import Base
+
  
-class ContaBancaria:
-    def __init__(self, titular:str):
-        self.__titular = titular
-        self.__saldo = 0
-   
-    @property
-    def saldo(self):
-        return self.__saldo
-   
-    def depositar(self, valor):
-        if valor <= 0:
-            return 'Valor invalido'
-        self.__saldo += valor
+DATABASE_URL = 'sqlite:///jvvt.db'
+engine = create_engine(DATABASE_URL)
  
-    def sacar(self, valor):
-        if valor <= self.__saldo:
-            self.__saldo -= valor
-        if valor < 0:
-            return 'Valor negativo'
-        else:
-            return 'Saldo insuficiente'
-       
- 
-class TestContaBancaria(unittest.TestCase):
- 
-    def test_saldo(self):
-        conta = ContaBancaria('João')
-        self.assertEqual(conta.saldo, 0)
- 
-    def test_depositar_valor_positivo(self):
-        conta = ContaBancaria('João')
-        conta.depositar(50)
-        self.assertEqual(conta.saldo, 50)
-       
-    def test_depositar_valor_invalido(self):
-        conta = ContaBancaria('João')
-        resultado = conta.depositar(-10)
-        self.assertEqual(resultado, 'Valor invalido')
- 
-    def test_sacar_valor_valido(self):
-        conta = ContaBancaria('João')
-        conta.depositar(50)
-        conta.sacar(20)
-        self.assertEqual(conta.saldo, 30)
- 
-    def test_sacar_valor_invalido(self):
-        conta = ContaBancaria('João')
-        resultado = conta.sacar(-1)
-        self.assertEqual(resultado, 'Valor negativo')
- 
-    def test_sacar_saldo_insuficiente(self):
-        conta = ContaBancaria('João')
-        resultado = conta.sacar(2000)
-        self.assertEqual(resultado, 'Saldo insuficiente')
- 
-unittest.main()
+Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,37 +1,11 @@
 
-import unittest
+from sqlalchemy import create_engine
 
-class Calculadora:
-    def soma(self, a, b):        
-        return a + b
-
-    def soma_com_valor_adicional(self, a, b):
-        valor_adicional = 2
-        return self.soma(a, b) + valor_adicional
-
-    def testeBoleano(self, valor):
-        return valor > 0  # Retorna True se o valor for maior que 0, caso contrário retorna False
+from model import Base
 
 
+DATABASE_URL = "sqlite:///database.db"  
 
+engine = create_engine(DATABASE_URL)
 
-class TesteCalculadora(unittest.TestCase):
-
-    def test_soma(self):
-        calcculadora = Calculadora()
-        resultado = calcculadora.soma(2, 3)
-        self.assertEqual(resultado, 5)
-
-
-    def test_soma_com_valor_adicional(self):
-        calculadora = Calculadora()
-        resultado = calculadora.soma_com_valor_adicional(2, 3)
-        self.assertEqual(resultado, 7)  # Espera-se que o resultado seja 7 devido ao valor adicional de 2
-
-    def test_boleano(self):
-        calculadora = Calculadora()
-        resultado = calculadora.testeBoleano(10)
-        self.assertTrue(resultado)  # Verifica se o resultado é maior que 0 
-
-unittest.main()
-   
+Base.metadata.create_all(engine)
